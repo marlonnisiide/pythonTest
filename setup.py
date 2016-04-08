8485e24232f5ccbca72d02e75f12c7989e4f0ff7
marlon <marlonnisiide@gmail.com>
2016-04-07 21:42:58 -0300

    testetet

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rangel_time',
+    version='0.1',
+    author='Marlon',
+    author_email='marlon.nisiide@agriness.com',
+    url='www.marlon.com',
+    py_modules=['rangel_time'],
+    )
